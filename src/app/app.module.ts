8b37089da9dbbdb66bc858b4695ef1a9c9c2279d
Santiago Pardo <tomas.gieco@hotmail.com>
2022-06-20 15:13:16 -0300

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent, 
+    EmployeeComponent, 
+    EmployeesComponent
+   ],
   imports: [
     BrowserModule
   ],
---
+++ src/app/employees/employees.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'appEmployees',
+    templateUrl: './employees.component.html',
+    styleUrls: ['./employees.component.css']
+})
+
+export class EmployeesComponent{
+
+}
